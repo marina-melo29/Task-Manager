f4ae7c5da067b28b9815bd9ad6287663285ffaba
marina-melo29 <ninadsm29@gmail>
2022-02-14 13:10:55 -0300

    adding #user_logged_in method and refactoring the authorization

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe 'Users V1 API', type: :request do
   let(:user_id) { user.id }
   let(:headers) do
     {
-      'Accept' => 'application/vnd.taskmanager.v1',
-      'Content-Type' => Mime[:json].to_s
+      'Accept'        => 'application/vnd.taskmanager.v1',
+      'Content-Type'  => Mime[:json].to_s,
+      'Authorization' => user.auth_token
     }
   end
 
