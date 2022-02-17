65cb23a42331a860084e2f550a417f47561b4070
marina-melo29 <ninadsm29@gmail>
2022-02-17 19:41:09 -0300

    Adding show action to the tasks controller

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -29,4 +29,19 @@ RSpec.describe 'Task API' do
 
   end
 
+  describe 'GET /tasks/:id' do
+    let(:task) { create(:task, user_id: user.id) }
+    before do
+      get "/tasks/#{task.id}", params: {}, headers: headers
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns json for tasks' do
+      expect(json_body[:title]).to eq(task.title)
+    end
+  end
+
 end
