2cd3eb7ee12a385d789e71f2abbf384191de498e
Marina <ninadsm29@gmail.com>
2022-02-27 12:27:28 -0300

    refact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #protect_from_forgery prepend: true
+  protect_from_forgery with: :null_session
 
   include Authenticable
 
